1eb418bd2b4492a8dfd2d92f68a1f60b359f8592
Sam Limpic <samlimpic@gmail.com>
2021-05-20 13:58:08 -0600

    auth0 update

--- kanban.client/src/env.js
+++ kanban.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const domain = 'samwise.us.auth0.com'
-export const audience = 'https://test-api.com'
-export const clientId = 'mC2vIZZ2G5J7rtlFOcbUWkcQSygfUbBF'
+export const audience = 'https://bill-boards.com'
+export const clientId = 'ssSD5pdHt9oszX3ViMLEk7HXOVPmt6p2'
