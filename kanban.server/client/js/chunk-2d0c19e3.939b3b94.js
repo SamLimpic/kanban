08f326d05e54fa6d6587b0bff6dbfdf5a54e2618
Sam Limpic <samlimpic@gmail.com>
2021-06-12 13:18:30 -0600

    heroku

--- kanban.server/client/js/chunk-2d0c19e3.939b3b94.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c19e3"],{"478b":function(e,n,t){"use strict";t.r(n);var c=t("7a23");const a={class:"about"},u=Object(c["g"])("h1",null,"This is the about page",-1);function o(e,n,t,o,s,b){return Object(c["q"])(),Object(c["d"])("div",a,[u])}var s={name:"About"};s.render=o;n["default"]=s}}]);
